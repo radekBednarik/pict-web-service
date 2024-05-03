92b14a0ff19f48efffcffc8d18a74ba73551c8f9
radek.Bednarik <radek.bednarik@tesena.com>
2024-05-03 10:56:48 +0200

    dev: wip

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,32 @@
 import "dotenv/config";
+import { writeFile, mkdir } from "node:fs";
 import express from "express";
+import { v4 as u4 } from "uuid";
 import PictGenerator from "pwtg";
 
+const port = process.env.PORT || 3000;
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/generate", (req, res) => {
-  //
+  // handle content type
+  if (!req.accepts("application/x-www-form-urlencoded")) {
+    res.status(412).json({ error: { code: 412, message: "Wrong Content-Type header value." } });
+  }
+
+  if (!Object.prototype.hasOwnProperty.call(req.body, "data")) {
+    res.status(400).json({ error: { code: 400, message: "Data not provided in request body." } });
+  }
+
+  const data = req.body["data"];
+  // must save data so PICT can load them - this is perf penalty, but
+  // since PICT is CLI first tool, it cannot be avoided
+  const path = `/tmp/pict/${u4()}`;
+  writeFile(path, data, { encoding: "utf-8" }, () => {
+    const generator = new PictGenerator(path, "pict");
+  });
 });
 
 app.get("/", (req, res) => {
