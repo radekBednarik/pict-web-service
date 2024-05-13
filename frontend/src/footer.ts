2185bebd469a1b2c5d0ed24969afb0e262961914
radek.Bednarik <radek.bednarik@tesena.com>
2024-05-13 07:49:20 +0200

    dev: added mailto

--- frontend/src/footer.ts
+++ frontend/src/footer.ts
@@ -2,7 +2,11 @@ export function injectFooter(element: HTMLDivElement) {
   element.innerHTML = `
     <div class="bg-primary-subtle rounded">
       <p class="mt-3 pt-3 pb-3 text-primary text-end">
-        &copy 2024 Radek Bednarik &nbsp
+        &copy 2024
+        <a href="mailto:radek.bednarik@tesena.com" class="text-reset">
+          Radek Bednarik
+        </a>
+        &nbsp
       </p>
     </div>
   `;
