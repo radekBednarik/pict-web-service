c3a7c8ce4ee42b1c1d52f8786a830d9a29298808
radek.Bednarik <radek.bednarik@tesena.com>
2024-05-15 07:59:57 +0200

    dev: conversion funcs done

--- backend/src/utils/conversions.ts
+++ backend/src/utils/conversions.ts
@@ -4,17 +4,26 @@ import * as fs from "fs";
 /* load the codepage support library for extended support with older formats  */
 import * as cpexcel from "xlsx/dist/cpexcel.full.mjs";
 
+import { writeFile } from "node:fs/promises";
+import { resolve } from "path";
+
 xlsx.set_fs(fs);
 xlsx.set_cptable(cpexcel);
 
-export function jsonToXlsx(inputData: string, outputPath: string) {
+/**
+ * Simplifies PICT output JSON structure so that `xlsx` utils method
+ * can process it and saves to .xlsx file
+ *
+ * @param inputData string
+ * @param outputPath string
+ * @throws
+ * @returns void
+ **/
+export function saveJsonAsXlsx(inputData: string, outputPath: string): void {
   try {
     const input = JSON.parse(inputData);
-    // TODO: we will have to manually convert json structure with
-    // nested arrays to array of arrays of values,
-    // where items of first sub-array will be headers
-    // then use this input to `aoa_to_sheet` method
-    const worksheet = xlsx.utils.aoa_to_sheet(input);
+    const simplifiedInput = _simplifyJson(input);
+    const worksheet = xlsx.utils.aoa_to_sheet(simplifiedInput);
     const workbook = xlsx.utils.book_new(worksheet);
 
     xlsx.writeFile(workbook, outputPath);
@@ -23,16 +32,51 @@ export function jsonToXlsx(inputData: string, outputPath: string) {
   }
 }
 
-export function tsvToCsv(inputData: string) {
+/**
+ * Converts .txt PICT output (which is in reality .tsv type of file)
+ * to .csv by replacing all `\t` for `,`.
+ * Saves data to provided location.
+ * That is all :)
+ *
+ * @param inputData string
+ * @param filepath string
+ * @throws
+ * @returns Promise<void>
+ **/
+export async function saveTsvAsCsv(inputData: string, filepath: string): Promise<void> {
   try {
-    return inputData.trim().replaceAll("\t", ",");
+    const data = inputData.trim().replaceAll("\t", ",");
+    await writeFile(resolve(process.cwd(), filepath), data, { encoding: "utf-8" });
   } catch (error) {
     throw new Error(`func tsvToCsv failed: ${error}`);
   }
 }
-// TODO: this is for debugging
-(async () => {
-  const fs = await import("fs");
-  const inputFileString = fs.readFileSync("tests/test-data/json-output.json", { encoding: "utf-8" });
-  jsonToXlsx(inputFileString, "output.xlsx");
-})();
+
+interface PictPair {
+  key: string;
+  value: string | number;
+}
+type PictCombination = PictPair[];
+type PictJsonOutput = PictCombination[];
+type SimplifiedRow = Array<string | number>;
+type SimplifiedOutput = SimplifiedRow[];
+
+function _simplifyJson(pictOutput: PictJsonOutput): SimplifiedOutput {
+  const header = pictOutput[0].map((obj) => {
+    return obj.key;
+  });
+
+  const rows: SimplifiedOutput = [];
+
+  pictOutput.forEach((row) => {
+    const temp: SimplifiedRow = [];
+    row.forEach((obj) => {
+      temp.push(obj.value);
+    });
+    rows.push(temp);
+  });
+
+  rows.unshift(header);
+
+  return rows;
+}
