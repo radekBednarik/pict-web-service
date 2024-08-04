0d669485e3d15a6f4252c1593446dfa40fc20d92
radek.Bednarik <radek.bednarik@tesena.com>
2024-08-04 16:54:37 +0200

    fix: update and fix headers

--- backend/src/utils/headers.ts
+++ backend/src/utils/headers.ts
@@ -1,7 +1,7 @@
 export function setCSPValue() {
-  if (!process.env.CI) {
-    return "default-src 'self' localhost:4000 pict-web-service-backend.onrender.com www.clarity.ms 'unsafe-inline'; img-src * data:;";
+  if (process.env.PROD) {
+    return "default-src 'self' pict-web-service-backend.onrender.com www.clarity.ms 'unsafe-inline'; img-src * data:;";
   }
 
-  return "default-src 'self' pict-web-service-backend.onrender.com www.clarity.ms 'unsafe-inline'; img-src * data:;";
+  return "default-src 'self' localhost:4000 pict-web-service-backend.onrender.com www.clarity.ms 'unsafe-inline'; img-src * data:;";
 }
