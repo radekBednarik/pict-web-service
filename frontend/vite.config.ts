b62a34c3a6438c99a80cd14c1be70629421773a7
radek.Bednarik <radek.bednarik@tesena.com>
2024-08-04 19:07:01 +0200

    sec: remove vite config content
    too much of hassle for this simple static site

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,4 @@
 import { defineConfig } from "vite";
-import { randomUUID } from "node:crypto";
 import { setCSP } from "./utils/headers";
 
-export default defineConfig({
-  html: {
-    cspNonce: randomUUID(),
-  },
-  server: {
-    headers: {
-      "content-security-policy": setCSP(),
-      "x-frame-options": "DENY",
-      "strict-transport-security":
-        "max-age=63072000; includeSubDomains; preload",
-    },
-  },
-});
+export default defineConfig({});
